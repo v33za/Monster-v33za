e936458f3435f4987644b2303fd4baa7abd1980d
AjMing <62439174+AjMing@users.noreply.github.com>
2022-05-31 04:48:19 +0000

    last change

--- .ccls-cache/@home@runner@Monster/main.cpp
+++ .ccls-cache/@home@runner@Monster/main.cpp
@@ -6,4 +6,10 @@ int main() {
   monster m("Strange",10,20);
   //monster x[10]={{"JJ",20,20},{"Burin"}};
   monster a("JJ",20,20),b("Sittipoj");
+  
+    monster *p;
+  p=new monster[3];
+  // how to create a monster object for p
+  // how to delete 
+  delete []p;
 }
